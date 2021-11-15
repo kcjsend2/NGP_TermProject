9e67abcd55097186da101608f791069e188ba171
kcjsend2 <kcjsend2@naver.com>
2021-11-15 17:51:28 +0900

    구조체 및 패킷 비트마스크 정의

--- NGP_Prototype_Server/Prototype_Server.h
+++ NGP_Prototype_Server/Prototype_Server.h
@@ -10,26 +10,37 @@
 #include <array>
 #include <iostream>
 
-// 패킷
-#define GAME_START      0b000001
-#define GAME_OVER       0b000010
-#define PLAYER_MOVE     0b000100
-#define PLAYER_HIT      0b001000
-#define BULLET_UPDATE   0b010000
-#define BULLET_DELETE   0b100000
+//패킷
+#define GAME_START 0b00001
+#define GAME_OVER 0b00010
+#define PLAYER_UPDATE 0b00100
+#define PLAYER_HIT 0b01000
+#define BULLET_DELETED 0b10000
 
-// 다른 클라이언트에 대한 정보 구조체
 #pragma pack(1)
 struct PlayerData
 {
-    int         m_id;               // 고유 번호
-    XMFLOAT3    m_position; 	    // 위치
-    XMFLOAT3    m_rotate;		    // 회전 정보(roll, pitch, yaw)
-    bool        m_bulletIsValid;    // 총알이 유효한지
-    XMFLOAT3    m_bulletPosition;	// 총알 위치
+	XMFLOAT3 m_position; 	// 플레이어 위치
+	XMFLOAT3 m_rotate;		// 플레이어 회전 정보(roll, pitch, yaw)
+	int m_life;			// 플레이어의 목숨 수
+	bool m_bHasBullet;		// 총알 유무
+	XMFLOAT3 m_bulletPosition;	// 총알 위치
 
-    PlayerData() : m_id{}, m_position{}, m_rotate{}, m_bulletIsValid{}, m_bulletPosition{}
-    {
+	PlayerData()
+	{
+		m_position = { 0.0f, 0.0f, 0.0f };
+		m_rotate = { 0.0f, 0.0f, 0.0f };
+		m_life = 0;
+		m_bHasBullet = false;
+		m_bulletPosition = { 0.0f, 0.0f, 0.0f };
+	}
 
-    }
+	PlayerData(XMFLOAT3 Position, XMFLOAT3 rotate, int life, bool hasBullet, XMFLOAT3 bulletPosition)
+	{
+		m_position = Position;
+		m_rotate = rotate;
+		m_life = life;
+		m_bHasBullet = hasBullet;
+		m_bulletPosition = bulletPosition;
+	}
 };
